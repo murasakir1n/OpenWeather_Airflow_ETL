d45ae0ad8e5420f395bfef5a12a3b90b927c5239
murasakir1n <jevven1223@gmail.com>
2026-06-01 20:15:28 +0200

    minor changes

--- weather_analytics/models/marts/humidity_pressure_stats.sql
+++ weather_analytics/models/marts/humidity_pressure_stats.sql
@@ -1,8 +1,7 @@
 select
     capital,
-    date(fetched_at) as weather_date,
     round(avg(humidity)::numeric, 2) as avg_humidity,
     round(avg(pressure)::numeric, 2) as avg_pressure
 from {{ source('weather_data', 'openweather_europe_etl') }}
-group by capital, date(fetched_at)
-order by capital, weather_date
+group by capital
+order by capital
